1e31108675e3001389fc8faeee8272ad6a31d054
Muaz <muaz.m.abboud@gmail.com>
2024-10-06 10:19:56 +0400

    sunday

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+function Footer(){
+    return(
+        <nav>
+            <article>
+                <h2>Dormat Navigation</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Menu</li>
+                    <li>Reservations</li>
+                    <li>Order Online</li>
+                    <li>Login</li>
+                </ul>
+
+            </article>
+            <article>
+                <h2>contact</h2>
+                <ul>
+                    <li>Adress</li>
+                    <li>Phone Number</li>
+                    <li>Email</li>
+                </ul>
+            </article>
+            <article>
+                <h2>social Medaia Links</h2>
+                <ul>
+                    <li>Facebook</li>
+                    <li>Twitter</li>
+                    <li>Youtube</li>
+                </ul>
+
+            </article>
+
+        </nav>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../img/logo.png";
+function Header(){
+    return (
+        <header>
+            <img src={logo} />.
+        </header>
+    )
+}
+
+export default Header;
